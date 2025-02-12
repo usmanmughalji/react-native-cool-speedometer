13a0c5405f3424cfbf0cb37562eb0212f79e69a7
usmanmughalji <usmanmughalji@gmail.com>
2025-02-12 17:17:37 +0500

    add TypeScript support and new components for speedometer

---
+++ dist/AnimatedSpeedometer.d.ts
@@ -0,0 +1,12 @@
+import { EasingFunction } from 'react-native';
+import { SpeedometerProps } from './Speedometer';
+interface AnimatedSpeedometerProps extends SpeedometerProps {
+    preFill?: number;
+    duration?: number;
+    easing?: EasingFunction;
+    onFillChange?: (value: number) => void;
+    onAnimationComplete?: () => void;
+    useNativeDriver?: boolean;
+}
+export default function AnimatedSpeedometer({ duration, easing, preFill, useNativeDriver, onFillChange, onAnimationComplete, value, ...rest }: AnimatedSpeedometerProps): JSX.Element;
+export {};
---
+++ dist/Progress.d.ts
@@ -0,0 +1,8 @@
+import { PathProps } from 'react-native-svg';
+interface ProgressProps extends PathProps {
+    color?: string;
+    arcWidth?: number;
+    lineCap?: PathProps['strokeLinecap'];
+}
+export default function Progress({ color, arcWidth, lineCap, ...rest }: ProgressProps): JSX.Element;
+export {};
